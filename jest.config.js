3e8262276f7b7943327d8dc63c6b851bc697697c
Connor Bär <connor.baer@me.com>
2021-03-31 00:39:16 +0200

    Archive repository

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  clearMocks: true,
-  rootDir: 'src',
-  coverageDirectory: './__reports__',
-  coveragePathIgnorePatterns: ['node_modules'],
-  testEnvironment: 'node',
-  setupFilesAfterEnv: ['jest-extended'],
-};
